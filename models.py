7d14b0a158bbbc77e9b1ac77d4d8d9ba6a262726
Dmitrii Zakharov <91263222+r1p71d3@users.noreply.github.com>
2024-12-03 10:42:50 -0500

    Update models.py (timestamp)

--- models.py
+++ models.py
@@ -6,12 +6,13 @@ from database import Base
 class Transaction(Base):
     __tablename__ = "transactions"
     id = Column(Integer, primary_key=True)
-    file_name = Column(String, index=True)
+    file_name = Column(String)
     file_hash = Column(String)
     tr_hash = Column(String, unique=True, index=True)
     bc_hash_link = Column(String, unique=True)
     bc_file_link = Column(String, unique=True)
     decrypt_key_first_last_5 = Column(String)
+    timestamp = Column(String)
     user_id = Column(Integer, ForeignKey("users.id"))
 
     user = relationship("User", back_populates="transactions")
